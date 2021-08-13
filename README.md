21c62f158cd9ed70fca349229dc16133908f39ca
Johannes Hauschild <git@johannes-hauschild.de>
2021-08-13 11:47:16 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# my awesome project using TeNPy
+
+This is a small template repository to simplify setting up projects that use TeNPy.
+To get started:
+[ ] Create a fork of this repository e.g. on github
+[ ] `git clone` your new project repository on your computer/on the cluster
+[ ] `git submodule init` to download the TeNPy repository
+
+
+## Updating the TeNPy version
+
+
+
+
+
---
+++ custom_model.py
@@ -0,0 +1,23 @@
+"""This file serves as an example how to extend TeNPy with custom classes.
+
+Here, we add an extra model, but you can also implement other custom classes like a Lattice, Site
+or Simulation class.
+"""
+
+import tenpy
+from tenpy.models.spins import SpinChain
+
+class AlternatingHeisenbergChain(SpinChain):
+    """Extension of the SpinChain"""
+
+    def init_terms(self, model_params):
+        J1 = model_params.get('J1', 1.)
+        J2 = model_params.get('J2', 0.5)
+
+        N_couplings = self.lat.Ls[0] - (self.lat.boundary_conditions[0] == 'open')
+        J = ([J1, J2] * N_couplings)[:N_couplings]
+        model_params['Jx'] = J
+        model_params['Jy'] = J
+        model_params['Jz'] = J
+        super().init_terms(model_params)
+
